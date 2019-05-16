44fd3b040f87d37843196888a6b32f91005f029e
Lexomir <lexomir@gmail.com>
2019-05-16 12:14:33 -0400

    -force hide Backstage collection when you move an item backstage

--- utils.py
+++ utils.py
@@ -66,10 +66,10 @@ def move_backstage(obj):
     backstage = bpy.data.collections.get("Backstage")
     if not backstage:
         backstage = bpy.data.collections.new("Backstage")
-        backstage.hide_viewport = True
     if not "Backstage" in bpy.context.scene.collection.children:
         bpy.context.scene.collection.children.link(backstage)
 
+    backstage.hide_viewport = True
     backstage.objects.link(obj)
 
 def move_onstage(obj):
